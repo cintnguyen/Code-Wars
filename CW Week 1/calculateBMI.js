0fd31377e91126f936e3e1104cbaed5dce3dac8f
cintnguyen <cintnguyen98@gmail.com>
2023-03-19 21:44:46 -0400

    calculate BMI first  commit

---
+++ CW Week 1/calculateBMI.js
@@ -0,0 +1,25 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+  // if bmi <= 18.5 return "Underweight"
+  
+  // if bmi <= 25.0 return "Normal"
+  
+  // if bmi <= 30.0 return "Overweight"
+  
+  // if bmi > 30 return "Obese"
+
+  function bmi(weight, height) {
+    let sum = weight /(height * height)
+    if (sum <= 18.5){
+      return "Underweight"
+    }
+    else if (sum <= 25.0){
+      return "Normal"
+    }
+    else if (sum <=30.0){
+      return "Overweight"
+    }
+    else{
+    return "Obese"
+    }
+  }
