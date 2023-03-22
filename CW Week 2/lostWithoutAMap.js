48b3139c08d30c815fb89fc7575af221900862bd
cintnguyen <cintnguyen98@gmail.com>
2023-03-22 02:03:04 -0400

    daily coding challenge

---
+++ CW Week 2/lostWithoutAMap.js
@@ -0,0 +1,8 @@
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    return x.map(num => num*2)
+   }
