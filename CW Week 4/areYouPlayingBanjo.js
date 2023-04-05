996bd5b50e4b932ca25b574d1506f51d4f767ca6
cintnguyen <cintnguyen98@gmail.com>
2023-04-05 18:06:05 -0400

    daily coding challenge

---
+++ CW Week 4/areYouPlayingBanjo.js
@@ -0,0 +1,18 @@
+// 8 kyu
+// Are You Playing Banjo?
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0) === "R" || name.charAt(0) === "r"){
+      return `${name} plays banjo`
+    } else{
+      return `${name} does not play banjo`
+    }
+  
+  }
