69cd6e6ffe96850061bda5b3bbc79442f37a8318
cintnguyen <cintnguyen98@gmail.com>
2023-04-12 12:04:28 -0400

    daily coding challenge

---
+++ CW Week 5/keepUpTheHoop.js
@@ -0,0 +1,15 @@
+// 8 kyu
+// Keep up the hoop
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount (n) {
+    if(n >= 10){
+      return "Great, now move on to tricks"
+    }
+   else{
+     return "Keep at it until you get it"
+   }
+  }
