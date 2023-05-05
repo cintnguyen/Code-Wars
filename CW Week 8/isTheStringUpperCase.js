997b71d4d323baff7f4cbf583efe38c8d0912811
cintnguyen <cintnguyen98@gmail.com>
2023-05-04 22:16:18 -0400

    daily coding challenge

---
+++ CW Week 8/isTheStringUpperCase.js
@@ -0,0 +1,16 @@
+  // 8 kyu
+  // Is the string uppercase?
+
+  // Create a method to see whether the string is ALL CAPS.
+
+  // Examples (input -> output)
+  // "c" -> False
+  // "C" -> True
+  // "hello I AM DONALD" -> False
+  // "HELLO I AM DONALD" -> True
+  // "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+  // "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+  
+  String.prototype.isUpperCase = function() {
+    return this.toString() === this.toUpperCase()
+  }
