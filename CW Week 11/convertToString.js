5208921cc5f28ead89ab63fd88cdaf8f5936f60b
cintnguyen <cintnguyen98@gmail.com>
2023-05-23 14:37:12 -0400

    daily coding challenge

---
+++ CW Week 11/convertToString.js
@@ -0,0 +1,14 @@
+// 8 kyu
+// Convert a Number to a String!
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+    return num.toString()
+}
