9943d84c9fdc4c833d370cc108e04a8b21ce3ab0
cintnguyen <cintnguyen98@gmail.com>
2023-05-29 11:20:10 -0400

    daily coding challenge

---
+++ CW Week 12/oppositeNumber.js
@@ -0,0 +1,13 @@
+// 8 kyu
+// Opposite number
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    return number * -1
+}
