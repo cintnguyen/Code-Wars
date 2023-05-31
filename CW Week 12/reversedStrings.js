8a93b8c28da78ffd221518e4912871deddf9738c
cintnguyen <cintnguyen98@gmail.com>
2023-05-31 15:19:41 -0400

    daily coding challenge

---
+++ CW Week 12/reversedStrings.js
@@ -0,0 +1,10 @@
+// 8 kyu
+// Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  => 'dlrow'
+// 'word'   => 'drow'
+
+function solution(str) {
+    return str.split("").reverse().join("")
+}
