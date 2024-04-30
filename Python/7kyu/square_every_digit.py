4b0d15840b2a29ad0a83694a516e1129ffd6c647
cintnguyen <cintnguyen98@gmail.com>
2024-04-30 02:30:03 -0400

    python coding challenge square every digit

---
+++ Python/7kyu/square_every_digit.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Square Every Digit
+
+# Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+def square_digits(num):
+    arr = str(num)
+    fin = []
+    for x in arr:
+        fin.append(str(int(x)**2))
+    return int("".join(fin))
