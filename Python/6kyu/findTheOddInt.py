706765b586fccb23b4c5569cfeac8fe5ac24f00d
cintnguyen <cintnguyen98@gmail.com>
2024-05-01 17:28:16 -0400

    python coding challenge find the odd int

---
+++ Python/6kyu/findTheOddInt.py
@@ -0,0 +1,21 @@
+# 6 KYU Find the odd int
+
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+# Examples
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+def find_it(seq):
+    arrNum = {}
+    for val in seq:
+        arrNum[val] = arrNum.get(val,0) + 1
+    
+    for num in arrNum:
+        if arrNum[num] % 2 != 0:
+            return num
