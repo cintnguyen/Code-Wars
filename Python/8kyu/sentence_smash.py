1ec56876d734d1a6b627c2cc7dc3fec1495751f3
cintnguyen <cintnguyen98@gmail.com>
2024-05-08 21:53:58 -0400

    python coding challenge sentence smash

---
+++ Python/8kyu/sentence_smash.py
@@ -0,0 +1,7 @@
+# 8 kyu
+# Sentence Smash
+
+# Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+def smash(words):
+    return " ".join(words)
