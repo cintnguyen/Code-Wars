0e8e9d1b8ac754692fcf99c1d8a136d03739218d
cintnguyen <cintnguyen98@gmail.com>
2024-10-16 03:58:23 -0400

    javascript coding challenge fake binary

---
+++ Javascript/8kyu/fakeBinary.js
@@ -0,0 +1,25 @@
+// 8 kyu
+// Fake Binary
+
+// Description:
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+function fakeBin(x){
+    let arr = x.split("")
+    let newArr =[]
+    arr.forEach(x => {
+      if (x < 5){
+        newArr.push(0)
+      }else{
+        newArr.push(1)
+      }
+    })
+    return newArr.join("")
+}
