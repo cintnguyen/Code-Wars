f20e3cf3e831dd29999ad3cd23f3998d32b283ff
cintnguyen <cintnguyen98@gmail.com>
2024-10-22 00:06:44 -0400

    javascript coding challenge switch it up

---
+++ Javascript/8kyu/switchItUp.js
@@ -0,0 +1,60 @@
+// 8 kyu
+// Switch it Up!
+
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+function switchItUp(number){
+    let numArr = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    return number ? numArr[number] : numArr[number]
+}
+
+switchItUp = n => ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+
+function switchItUp(number) {
+    switch (number) {
+      case 0: return 'Zero';
+      case 1: return 'One';
+      case 2: return 'Two';
+      case 3: return 'Three';
+      case 4: return 'Four';
+      case 5: return 'Five';
+      case 6: return 'Six';
+      case 7: return 'Seven';
+      case 8: return 'Eight';
+      case 9: return 'Nine';
+    }
+}
+
+// Different switch case format
+
+function switchItUp(number){
+    switch(number) {
+      case 0:
+        return 'Zero';
+      case 1:
+        return 'One';
+      case 2:
+        return 'Two';
+      case 3:
+        return 'Three';
+      case 4:
+        return  'Four';
+      case 5:
+        return 'Five';
+      case 6:
+        return 'Six';
+      case 7:
+        return 'Seven';
+      case 8:
+        return 'Eight';
+      case 9:
+        return 'Nine';
+      default:
+        return 'Unknown number';
+    }
+}
