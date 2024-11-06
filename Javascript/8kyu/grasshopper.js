baaa9cf9dc61cb33c6f7fec9060710e2887c0572
cintnguyen <cintnguyen98@gmail.com>
2024-11-05 20:54:46 -0500

    javascript coding challenge grasshopper debug

---
+++ Javascript/8kyu/grasshopper.js
@@ -0,0 +1,15 @@
+// 8 kyu
+// Grasshopper - Debug sayHello
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+    return `Hello, ${name}`
+}
+
+const sayHello = name => `Hello, ${name}`;
