44c4f24a2d25cb35fcde80a7339edc90df934369
cintnguyen <cintnguyen98@gmail.com>
2024-11-08 22:52:27 -0500

    javascript coding challenge calculate excluding vat

---
+++ Javascript/8kyu/calculateExcludingVat.js
@@ -0,0 +1,26 @@
+// 8 kyu
+// Calculate Price Excluding VAT
+
+// Write a function that calculates the original product price, without VAT.
+
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+// Thus, if your function receives 230.00 as input, it should return 200.00
+
+// Notes:
+
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1
+
+excludingVatPrice = p => p === null ? -1 : +(p / 1.15).toFixed(2);
+
+function excludingVatPrice(price){
+    if(price){
+        let vat = price - price/1.15
+        return Number((price - vat).toFixed(2))
+    }else{
+      return -1
+    }
+}
