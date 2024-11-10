3ce6a5e0f9623caf30dd36c5a7ad5d86529b4de4
cintnguyen <cintnguyen98@gmail.com>
2024-11-10 02:02:53 -0500

    javascript coding challenge convert a string to an array

---
+++ Javascript/8kyu/convertStringToArr.js
@@ -0,0 +1,13 @@
+// 8 kyu
+// Convert a string to an array
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+    return string.split(" ")
+}
