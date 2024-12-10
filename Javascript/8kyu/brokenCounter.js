65927253666d0003ae553b1cc5da3f1e89999989
cintnguyen <cintnguyen98@gmail.com>
2024-12-09 22:33:20 -0500

    javascript coding challenge broken counter

---
+++ Javascript/8kyu/brokenCounter.js
@@ -0,0 +1,22 @@
+// 8 kyu
+// Broken Counter
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+// Solution - In JavaScript, when adding methods to a prototype, the syntax does not use parentheses (() after the method name). Your code mistakenly uses Counter.prototype.increase() instead of Counter.prototype.increase
+
+function Counter() {
+    this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+    this.value++;
+};
+
+Counter.prototype.getValue = function () {
+    return this.value;
+};
+
+Counter.prototype.reset = function () {
+    this.value = 0;
+};
