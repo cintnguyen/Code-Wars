5df2d5c71b284b18b883b8c112e0ab12dd233634
cintnguyen <cintnguyen98@gmail.com>
2024-12-10 22:35:57 -0500

    javascript coding challenge broken counter

--- Javascript/8kyu/brokenCounter.js
+++ Javascript/8kyu/brokenCounter.js
@@ -20,3 +20,15 @@ Counter.prototype.getValue = function () {
 Counter.prototype.reset = function () {
     this.value = 0;
 };
+
+
+
+function Counter() {
+    this.value = 0;
+  }
+  
+  Counter.prototype = {
+    increase : function(){this.value++;},
+    getValue : function(){ return this.value;},
+    reset : function(){this.value = 0;}
+};
