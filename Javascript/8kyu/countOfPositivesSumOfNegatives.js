21077072d0ac8dcc925c0da3ff0e6765fca45824
cintnguyen <cintnguyen98@gmail.com>
2024-12-23 02:56:50 -0500

    javascript coding challenge count of positives sum of negatives

--- Javascript/8kyu/countOfPositivesSumOfNegatives.js
+++ Javascript/8kyu/countOfPositivesSumOfNegatives.js
@@ -25,3 +25,7 @@ function countPositivesSumNegatives(input) {
     })
     return [pos, neg]
 }
+
+function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
