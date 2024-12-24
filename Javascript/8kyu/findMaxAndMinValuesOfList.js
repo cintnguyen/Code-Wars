0861d47fb3b3479a242556e775a5d952ae5d1348
cintnguyen <cintnguyen98@gmail.com>
2024-12-24 03:06:32 -0500

    javascript coding challenge find max and min values of list

---
+++ Javascript/8kyu/findMaxAndMinValuesOfList.js
@@ -0,0 +1,30 @@
+// 8 kyu
+// Find Maximum and Minimum Values of a List
+
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively. Each function returns one number.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+const min = function(list){
+    let num = list[0]
+    list.forEach(x =>{
+        if(x < num){
+        num = x
+      }
+    }) 
+    return num
+}
+
+const max = function(list){
+    let num = list[0]
+    list.forEach(x =>{
+        if(x > num){
+        num = x
+      }
+    }) 
+    return num
+}
